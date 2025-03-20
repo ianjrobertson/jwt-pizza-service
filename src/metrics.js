b1e5e134b85242a58d25a2572ce5561df36d64a1
ianjrobertson <45133404+ianjrobertson@users.noreply.github.com>
2025-03-19 20:56:12 -0600

    Delete unused file

--- src/metrics.js
+++ src/metrics.js
@@ -286,62 +286,6 @@ function pizzaLatencyMetrics(metrics) {
 
 //TODO in order for the sendMetric to work, we will need to set the correct metric value for different types. 
 
-function sendMetricToGrafana(metricName, metricValue, attributes) {
-  attributes = { ...attributes, source: config.metrics.source };
-
-   const metric = {
-    resourceMetrics: [
-      {
-        scopeMetrics: [
-          {
-            metrics: [
-              {
-                name: metricName,
-                unit: '1',
-                sum: {
-                  dataPoints: [
-                    {
-                      asInt: metricValue,
-                      timeUnixNano: Date.now() * 1000000,
-                      attributes: [],
-                    },
-                  ],
-                  aggregationTemporality: 'AGGREGATION_TEMPORALITY_CUMULATIVE',
-                  isMonotonic: true,
-                },
-              },
-            ],
-          },
-        ],
-      },
-    ],
-  };
-
-  Object.keys(attributes).forEach((key) => {
-    metric.resourceMetrics[0].scopeMetrics[0].metrics[0].sum.dataPoints[0].attributes.push({
-      key: key,
-      value: { stringValue: attributes[key] },
-    });
-  });
-
-  fetch(`${config.metrics.url}`, {
-    method: 'POST',
-    body: JSON.stringify(metric),
-    headers: { Authorization: `Bearer ${config.metrics.apiKey}`, 'Content-Type': 'application/json' },
-  })
-    .then((response) => {
-      if (!response.ok) {
-        console.error('Failed to push metrics data to Grafana');
-        console.log(response);
-      } else {
-        console.log(`Pushed ${metricName}`);
-      }
-    })
-    .catch((error) => {
-      console.error('Error pushing metrics:', error);
-    });
-}
-
 function sendMetric2Grafana(metrics) {
    const metric = {
     resourceMetrics: [
